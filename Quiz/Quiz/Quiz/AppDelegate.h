e373f04e7a5d981bc0a06acef678d33bb966e7fa
zsirui <zsirui@126.com>
2016-04-05 09:55:14 +0800

    Create Quiz Project

---
+++ Quiz/Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Ray on 4/5/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Ray on 4/5/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
